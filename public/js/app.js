04aad510a3c1eb5e286f1ce04d822aa07d5d5f86
altai <altai@ss-aca.jp>
2020-07-24 14:29:38 +0900

    xserver update

--- public/js/app.js
+++ public/js/app.js
@@ -49888,8 +49888,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /vagrant/rlf/manage/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /vagrant/rlf/manage/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /home/foodreborn01/gohan-tabi.com/public_html/mg/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /home/foodreborn01/gohan-tabi.com/public_html/mg/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
