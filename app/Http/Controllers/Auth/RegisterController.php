c27de14872cdc397a81f648301c6ddfbe8382306
matayoshi <matayoshi@sem-cloud.com>
2020-07-29 17:14:18 +0900

    ログ出力

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -9,6 +9,8 @@ use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 
+use Illuminate\Support\Facades\Log;
+
 class RegisterController extends Controller
 {
     /*
