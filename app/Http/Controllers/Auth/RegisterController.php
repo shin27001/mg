6d6d1cc0926eeba146f89c3be600ef077dd565fd
matayoshi <matayoshi@sem-cloud.com>
2020-07-29 17:11:52 +0900

    修正5

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -71,8 +71,28 @@ class RegisterController extends Controller
         ]);
     }
 
-    public function redirectPath()
-    {
-        return '/mypage';
-    }    
+
+    public function showRegistrationForm() {
+        if(isset($_SERVER['HTTP_REFERER'])) {
+            $intended = $_SERVER['HTTP_REFERER'];
+        } else {
+            $intended = '/';
+        }
+        Log::debug('デバッグメッセージ');
+        Log::debug($intended);
+        
+
+        session(['url.intended' => $intended]);
+        return view('auth.register');
+    }
+    
+    protected function registered(Request $request, $user) {
+        return redirect(session('url.intended'));
+        // return redirect('users/' . $user->id)->with('my_status', 'ユーザー登録はまただ完了していません。メールに記載されているリンクをクリックしてください。');
+    }
+
+    // public function redirectPath()
+    // {
+    //     return '/mypage';
+    // }    
 }
